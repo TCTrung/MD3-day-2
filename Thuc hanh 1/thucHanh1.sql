aa27541f354073fafb4bc87491681f232e0515da
TCTrung <trungtrinh1325@gmail.com>
2023-11-17 13:50:09 +0700

    hi

---
+++ Thuc hanh 1/thucHanh1.sql
@@ -0,0 +1,29 @@
+create database `quanLyDiemThi`;
+use `quanLyDiemThi`;
+create table HocSinh(
+MaHS varchar(20) PRIMARY KEY,
+TenHS varchar(50),
+ngaySinh datetime,
+lop varchar(20),
+GT varchar(20)
+);
+CREATE TABLE MonHoc(
+ MaMH VARCHAR(20) PRIMARY KEY,
+ TenMH VARCHAR(50),
+ MaGV VARCHAR(20)
+);
+CREATE TABLE BangDiem(
+ MaHS VARCHAR(20),
+ MaMH VARCHAR(20),
+ DiemThi INT,
+ NgayKT DATETIME,
+ PRIMARY KEY (MaHS, MaMH),
+ FOREIGN KEY (MaHS) REFERENCES HocSinh(MaHS),
+ FOREIGN KEY (MaMH) REFERENCES MonHoc(MaMH)
+);
+CREATE TABLE GiaoVien(
+ MaGV VARCHAR(20) PRIMARY KEY,
+ TenGV VARCHAR(20),
+ SDT VARCHAR(10)
+);
+ALTER TABLE MonHoc ADD CONSTRAINT FK_MaGV FOREIGN KEY (MaGV) REFERENCES GiaoVien(MaGV);
